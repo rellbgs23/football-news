21809afa217f6bbe385ac7de51cda384f483be31
Kevinistic <appealergt@gmail.com>
2025-09-27 15:52:13 +0700

    Tutorial 2

--- football_news/settings.py
+++ football_news/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -143,3 +143,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://pbp.cs.ui.ac.id/farrel.rifqi/footballnews"
+]
